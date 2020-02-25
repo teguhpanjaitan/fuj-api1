ea387df5bc16744e0fa8e90716e3b68bb7372632
Teguh <teguhpanjaitan@gmail.com>
2020-02-25 22:17:28 +0700

    Home controller added

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return new Response('', Response::HTTP_OK);
+    }
+}
