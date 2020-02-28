69ac344881bbd4308ae8bd5d1116868519157cd7
Teguh <teguhpanjaitan@gmail.com>
2020-02-28 21:54:27 +0700

    Publish get and patch users and hide pass

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -2,10 +2,18 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
+ * @ApiResource(
+ *      collectionOperations={},
+ *      itemOperations={"get","patch"},
+ *      normalizationContext={"groups"={"read"}},
+ *      denormalizationContext={"groups"={"write"}}
+ * )
  * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
  */
 class Users implements UserInterface
@@ -19,11 +27,13 @@ class Users implements UserInterface
 
     /**
      * @ORM\Column(type="text", nullable=true)
+     * @Groups({"read", "write"})
      */
     private $bio;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"read", "write"})
      */
     private $username;
 
