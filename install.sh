262f17c6d0802b6640ced1f121003026b0312e3b
Teguh <teguhpanjaitan@gmail.com>
2020-03-09 09:45:05 +0700

    Add docker compose

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf var &&
+
+#keygen part
+mkdir -p "config/jwt" &&
+rm -f config/jwt/private.pem &&
+rm -f config/jwt/public.pem &&
+openssl genpkey -out config/jwt/private.pem -aes256 -algorithm rsa -pass pass:fuj-api1 -pkeyopt rsa_keygen_bits:4096 &&
+openssl pkey -in config/jwt/private.pem -out config/jwt/public.pem -pubout -passin pass:fuj-api1 &&
+
+##install all dependencies
+composer install &&
+tail -f /dev/null
